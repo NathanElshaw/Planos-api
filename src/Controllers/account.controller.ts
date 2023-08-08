4add1fd4b0b1bc7cb448a9ba798ecee472ca64c1
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-08-07 22:16:21 -0400

    Removed: return response

--- src/Controllers/account.controller.ts
+++ src/Controllers/account.controller.ts
@@ -3,7 +3,7 @@ import { Request, Response } from "express";
 export const Account_Handler = {
   Create_Account: async (req: Request, res: Response) => {
     try {
-      return res.send("Cookie");
+      return res.send("");
     } catch (e: any) {
       return res.status(409).send(e);
     }
