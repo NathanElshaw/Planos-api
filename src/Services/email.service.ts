7b1b455dff042703f6a0d642c8c2644b013aaff4
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-08-07 22:16:56 -0400

    Added: Email transporter and service handler

--- src/Services/email.service.ts
+++ src/Services/email.service.ts
@@ -1,3 +1,31 @@
 import nodemailer from "nodemailer";
+import "dotenv/config";
+import { Request, Response } from "express";
 
-let transporter = nodemailer.createTransport();
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAILUSER,
+    pass: process.env.EMAILPASS,
+  },
+});
+
+const Email_Services = {
+  Send_Verification_Service: async (user_Info: any) => {
+    try {
+      const info = transporter.sendMail({
+        from: `"Planos" <${process.env.EMAIL}>`,
+        to: `${user_Info.name} <${user_Info.email}>`,
+        subject: "Planos Verification Code",
+        text: "HI",
+        html: "<h1>HI</h1>",
+      });
+      console.log("Message Sent: %s", (await info).messageId);
+      return;
+    } catch (e: any) {
+      return e;
+    }
+  },
+};
+
+export default Email_Services;
