9b98a0875cfa7b64cb41dae9642d93d4a1d41df6
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-08-08 21:34:32 -0400

    Added: db connection

--- src/App.ts
+++ src/App.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import routes from "./Routes";
+import Db_Connect from "./Providers/DB_Provider";
 
 const app = express();
 
 app.use(express.json());
 
 app.listen(3000, async () => {
+  console.log(await Db_Connect());
   console.log("Running");
   routes(app);
 });
