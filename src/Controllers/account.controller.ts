9bf3cf04b64cc08d7dfb436c306434e931024551
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-08-08 21:34:48 -0400

    Added: create account handler

--- src/Controllers/account.controller.ts
+++ src/Controllers/account.controller.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from "express";
+import Account_Service from "../Services/account.service";
 
 export const Account_Handler = {
   Create_Account: async (req: Request, res: Response) => {
     try {
-      return res.send("");
+      return res.send(await Account_Service.Create_Account(req.body));
     } catch (e: any) {
       return res.status(409).send(e);
     }
