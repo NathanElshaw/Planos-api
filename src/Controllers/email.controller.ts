04d44e052447e9b586776914e05daa42d59af64a
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-08-09 23:17:35 -0400

    Added: resend function

--- src/Controllers/email.controller.ts
+++ src/Controllers/email.controller.ts
@@ -4,12 +4,14 @@ import Email_Services from "../Services/email.service";
 const Email_Handler = {
   Send_Verification: async (req: Request, res: Response) => {
     try {
-      return res.send(
-        await Email_Services.Send_Verification_Service({
-          name: "User",
-          email: "arrowpointmarketing@gmail.com",
-        })
-      );
+      return res.send(await Email_Services.Send_Verification(req.body));
+    } catch (e: any) {
+      return res.status(409).send(e);
+    }
+  },
+  Resend_Verification: async (req: Request, res: Response) => {
+    try {
+      return res.send(await Email_Services.Resend_Verification(req.body));
     } catch (e: any) {
       return res.status(409).send(e);
     }
