2f6ae4499cc02ff2f766704dbd8411462621c0ac
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-08-09 23:17:58 -0400

    Added: resend verification

--- src/Services/email.service.ts
+++ src/Services/email.service.ts
@@ -13,7 +13,7 @@ const transporter = nodemailer.createTransport({
 });
 
 const Email_Services = {
-  Send_Verification_Service: async (user_Info: Account_Info_Document) => {
+  Send_Verification: async (user_Info: Account_Info_Document) => {
     try {
       const info = transporter.sendMail({
         from: `"Planos" <${process.env.EMAIL}>`,
@@ -27,6 +27,27 @@ const Email_Services = {
         )}</h1>`,
       });
       console.log("Message Sent: %s", (await info).messageId);
+      return;
+    } catch (e: any) {
+      return e;
+    }
+  },
+  Resend_Verification: async (user_Info: Account_Info_Document) => {
+    try {
+      const Code_Check = await Account_Service.Check_For_Code(user_Info._id);
+      if (Code_Check === false)
+        return await Email_Services.Send_Verification(user_Info);
+      const info = transporter.sendMail({
+        from: `"Planos" <${process.env.EMAIL}>`,
+        to: `${user_Info.name} <${user_Info.email}>`,
+        subject: "Planos Verification Code",
+        text: `${await Account_Service.Create_Verification_Code(
+          user_Info.email
+        )}`,
+        html: `<h1>${await Account_Service.Create_Verification_Code(
+          user_Info.email
+        )}</h1>`,
+      });
       return;
     } catch (e: any) {
       return e;
