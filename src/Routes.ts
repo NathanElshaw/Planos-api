440c69005ec518fc719e16d302a513b59cc10177
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-08-10 23:15:59 -0400

    Added: spam prevent for testing

--- src/Routes.ts
+++ src/Routes.ts
@@ -1,9 +1,10 @@
 import { Express } from "express";
 import { Account_Handler } from "./Controllers/account.controller";
 import Email_Handler from "./Controllers/email.controller";
+import Spam_Prevent from "./MiddleWare/spamPrevent";
 
 const Routes = (app: Express) => {
-  app.get("/api", Email_Handler.Send_Verification);
+  app.get("/api", Spam_Prevent);
 };
 
 export default Routes;
