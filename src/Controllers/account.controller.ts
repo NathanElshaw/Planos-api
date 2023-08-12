d0f4973c8c3f4c4529c337e93ca8d639d87b43d2
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-08-11 21:38:01 -0400

    Added: add code to account

--- src/Controllers/account.controller.ts
+++ src/Controllers/account.controller.ts
@@ -4,6 +4,10 @@ import Account_Service from "../Services/account.service";
 export const Account_Handler = {
   Create_Account: async (req: Request, res: Response) => {
     try {
+      const account = req.body;
+      Object.assign(account, {
+        code: await Account_Service.Create_Verification_Code(req.body),
+      });
       return res.send(await Account_Service.Create_Account(req.body));
     } catch (e: any) {
       return res.status(409).send(e);
