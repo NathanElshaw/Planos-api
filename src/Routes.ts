2b89db8df22879bc99398a262b235632fb5b9995
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-08-12 23:33:30 -0400

    Added: check to account making

--- src/Routes.ts
+++ src/Routes.ts
@@ -2,9 +2,11 @@ import { Express } from "express";
 import { Account_Handler } from "./Controllers/account.controller";
 import Email_Handler from "./Controllers/email.controller";
 import Spam_Prevent from "./MiddleWare/spamPrevent";
+import Signup_Check_Handler from "./Controllers/signup.error.controller";
 
 const Routes = (app: Express) => {
-  app.get("/api", Account_Handler.Create_Account);
+  app.get("/api", Signup_Check_Handler, Account_Handler.Create_Account);
+  app.delete("/api", Account_Handler.Delete_Account);
 };
 
 export default Routes;
