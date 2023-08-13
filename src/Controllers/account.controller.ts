afe2cd8c07a061fa32a17d970583bafdb46b4545
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-08-12 23:32:47 -0400

    Added: code to req and respond with send email

--- src/Controllers/account.controller.ts
+++ src/Controllers/account.controller.ts
@@ -1,14 +1,27 @@
 import { Request, Response } from "express";
 import Account_Service from "../Services/account.service";
+import Email_Services from "../Services/email.service";
 
 export const Account_Handler = {
   Create_Account: async (req: Request, res: Response) => {
     try {
-      const account = req.body;
-      Object.assign(account, {
-        code: await Account_Service.Create_Verification_Code(req.body),
+      Object.assign(req.body, {
+        code: null,
       });
-      return res.send(await Account_Service.Create_Account(req.body));
+      return res.send(
+        await Account_Service.Create_Account(req.body).then(
+          async (response) => {
+            await Email_Services.Send_Verification(req.body);
+          }
+        )
+      );
+    } catch (e: any) {
+      return res.status(409).send(e);
+    }
+  },
+  Delete_Account: async (req: Request, res: Response) => {
+    try {
+      return res.send(await Account_Service.Delete_Account(req.body.email));
     } catch (e: any) {
       return res.status(409).send(e);
     }
