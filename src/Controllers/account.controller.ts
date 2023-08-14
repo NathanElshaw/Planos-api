835c7707a637365fd5a875a362aa80a1a8b7895f
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-08-13 21:10:08 -0400

    Added: encoding passwords

--- src/Controllers/account.controller.ts
+++ src/Controllers/account.controller.ts
@@ -1,13 +1,16 @@
 import { Request, Response } from "express";
 import Account_Service from "../Services/account.service";
 import Email_Services from "../Services/email.service";
+import { Account_Info_Document } from "../Models/account.model";
 
 export const Account_Handler = {
   Create_Account: async (req: Request, res: Response) => {
     try {
-      Object.assign(req.body, {
-        code: null,
-      });
+      Object.assign(
+        req.body,
+        { code: "" },
+        { password: Buffer.from(req.body.password, "ascii").toString("base64") }
+      );
       return res.send(
         await Account_Service.Create_Account(req.body).then(
           async (response) => {
@@ -19,6 +22,19 @@ export const Account_Handler = {
       return res.status(409).send(e);
     }
   },
+  Confirm_Code: async (req: Request, res: Response) => {
+    try {
+      const requesting: Account_Info_Document = req.body;
+      const code_Check = await Account_Service.Verify_Code(
+        requesting.email,
+        requesting.code as string
+      );
+      return res.send(code_Check === true ? true : "Invalid code");
+    } catch (e: any) {
+      return res.status(409).send(e);
+    }
+  },
+
   Delete_Account: async (req: Request, res: Response) => {
     try {
       return res.send(await Account_Service.Delete_Account(req.body.email));
