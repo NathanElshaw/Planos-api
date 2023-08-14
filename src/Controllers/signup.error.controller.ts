6dbaed07186dd170081fa417d7edc2d93acd9687
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-08-13 21:11:08 -0400

    Added: change to conditional checks

--- src/Controllers/signup.error.controller.ts
+++ src/Controllers/signup.error.controller.ts
@@ -1,21 +1,13 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 import SignUp_Check_Service from "../Services/signup.error.service";
 
-const Signup_Check_Handler = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+const Signup_Check_Handler = async (req: Request, res: Response) => {
   try {
-    const Email_Check = await SignUp_Check_Service.Check_Email(req.body.email);
-    const Username_Check = await SignUp_Check_Service.Check_Username(
-      req.body.username
-    );
-    return Email_Check === false
-      ? Username_Check === false
-        ? next()
-        : res.send("Username Exists")
-      : res.send("Email Exists");
+    return req.body.email
+      ? res.send(await SignUp_Check_Service.Check_Email(req.body.email))
+      : req.body.username
+      ? res.send(await SignUp_Check_Service.Check_Username(req.body.username))
+      : res.status(404).send("Check Failed");
   } catch (e: any) {
     return res.status(409).send(e);
   }
