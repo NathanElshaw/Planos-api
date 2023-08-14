cae0cca59ecd7dcce7ddce91e5db862e4d92b021
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-08-13 21:10:36 -0400

    Added: thank you email

--- src/Controllers/email.controller.ts
+++ src/Controllers/email.controller.ts
@@ -16,6 +16,13 @@ const Email_Handler = {
       return res.status(409).send(e);
     }
   },
+  Send_Thank_You: async (req: Request, res: Response) => {
+    try {
+      return res.send(await Email_Services.Send_SignUp_Thank_You(req.body));
+    } catch (e: any) {
+      return res.status(409).send(e);
+    }
+  },
 };
 
 export default Email_Handler;
