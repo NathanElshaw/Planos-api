bc2ca7b2ec72d20782abb080249b2aa5c504ad94
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-08-14 23:48:23 -0400

    Added: cookie parser

--- src/App.ts
+++ src/App.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import routes from "./Routes";
 import Db_Connect from "./Providers/DB_Provider";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json(), cookieParser());
 
 app.listen(3000, async () => {
   console.log(await Db_Connect());
