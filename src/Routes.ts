5a0b932e84d2b34c305f98c9cea6d84d0e104c26
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-08-14 23:50:11 -0400

    Added: route for api key

--- src/Routes.ts
+++ src/Routes.ts
@@ -3,12 +3,14 @@ import { Account_Handler } from "./Controllers/account.controller";
 import Email_Handler from "./Controllers/email.controller";
 import Spam_Prevent from "./MiddleWare/spamPrevent";
 import Signup_Check_Handler from "./Controllers/signup.error.controller";
+import Api_Keys_Handler from "./Controllers/keys.controller";
 
 const Routes = (app: Express) => {
   app.get("/api", Account_Handler.Create_Account);
   app.get("/api/check", Signup_Check_Handler);
   app.get("/api/verify_code", Account_Handler.Confirm_Code);
   app.delete("/api", Account_Handler.Delete_Account);
+  app.get("/api/createKey", Api_Keys_Handler.Create_Key);
 };
 
 export default Routes;
