8831cb4f4ba9383a3da5f7032e1c7fab0b248780
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-08-15 20:38:50 -0400

    Added: delete and check

--- src/Controllers/keys.controller.ts
+++ src/Controllers/keys.controller.ts
@@ -1,17 +1,29 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import Api_Keys_Service from "../Services/key.service";
 
 const Api_Keys_Handler = {
   Create_Key: async (req: Request, res: Response) => {
     try {
       const key = await Api_Keys_Service.Create();
-      return res.cookie("Api-Key", key, { httpOnly: true }).send();
+      return res.cookie("Api_Key", key, { httpOnly: true }).send();
     } catch (e: any) {
       return res.status(409).send(e);
     }
   },
-  Check_Key: async () => {},
-  Delete_Key: async () => {},
+  Check_Key: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      return !req.cookies?.Api_Key ? Api_Keys_Handler.Create_Key : next();
+    } catch (e: any) {
+      return res.status(409).send(e);
+    }
+  },
+  Delete_Key: async (req: Request, res: Response) => {
+    try {
+      return res.send(req.body._id);
+    } catch (e: any) {
+      return res.status(409).send(e);
+    }
+  },
 };
 
 export default Api_Keys_Handler;
