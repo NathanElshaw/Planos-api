d84b88e4da42dd4da76a10f7de51ce4b8a9a2b52
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-08-17 20:58:33 -0400

    Added: obj to error for proper rendering

--- src/Controllers/signup.error.controller.ts
+++ src/Controllers/signup.error.controller.ts
@@ -3,10 +3,16 @@ import SignUp_Check_Service from "../Services/signup.error.service";
 
 const Signup_Check_Handler = async (req: Request, res: Response) => {
   try {
-    return req.body.email
-      ? res.send(await SignUp_Check_Service.Check_Email(req.body.email))
-      : req.body.username
-      ? res.send(await SignUp_Check_Service.Check_Username(req.body.username))
+    return req.query.email
+      ? res.send(
+          await SignUp_Check_Service.Check_Email(req.query.email as string)
+        )
+      : req.query.username
+      ? res.send(
+          await SignUp_Check_Service.Check_Username(
+            req.query.username as string
+          )
+        )
       : res.status(404).send("Check Failed");
   } catch (e: any) {
     return res.status(409).send(e);
