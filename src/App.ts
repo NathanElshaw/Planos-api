869ffdaa3807dec8f8072e7d492152b6e66ca203
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-08-17 20:56:58 -0400

    Added: cors

--- src/App.ts
+++ src/App.ts
@@ -2,12 +2,19 @@ import express from "express";
 import routes from "./Routes";
 import Db_Connect from "./Providers/DB_Provider";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json(), cookieParser());
 
-app.listen(3000, async () => {
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.listen(3001, async () => {
   console.log(await Db_Connect());
   console.log("Running");
   routes(app);
