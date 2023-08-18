9351478c8e4398f1ff0144fad1101d0f35686c09
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-08-17 20:57:16 -0400

    Added: correct routes

--- src/Routes.ts
+++ src/Routes.ts
@@ -1,12 +1,10 @@
 import { Express } from "express";
 import { Account_Handler } from "./Controllers/account.controller";
-import Email_Handler from "./Controllers/email.controller";
-import Spam_Prevent from "./MiddleWare/spamPrevent";
 import Signup_Check_Handler from "./Controllers/signup.error.controller";
 import Api_Keys_Handler from "./Controllers/keys.controller";
 
 const Routes = (app: Express) => {
-  app.get("/api", Account_Handler.Create_Account);
+  app.post("/api", Account_Handler.Create_Account);
   app.get("/api/check", Signup_Check_Handler);
   app.get("/api/verify_code", Account_Handler.Confirm_Code);
   app.delete("/api", Account_Handler.Delete_Account);
